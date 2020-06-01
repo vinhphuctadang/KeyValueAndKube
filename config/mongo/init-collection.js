9bdb1403f9d5749d0e2a764b8446092c38a9f066
foo <vinhphuctadang@gmail.com>
2020-06-01 12:13:39 +0700

    changed: config of resources

--- config/mongo/init-collection.js
+++ config/mongo/init-collection.js
@@ -1,6 +1,5 @@
 sh.enableSharding('test')
 use test
-db.KeyValue.drop() // drop collection for creation safety
 db.createCollection('KeyValue')
 db.KeyValue.createIndex({key:1}, {unique: true})
 sh.shardCollection('test.KeyValue', {key: 'hashed'}, false, {numInitialChunks: 2})
