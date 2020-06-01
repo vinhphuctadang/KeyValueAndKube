9f462f380f7b63a07dc7acfd709ced523e10667b
foo <vinhphuctadang@gmail.com>
2020-06-01 10:08:45 +0700

    fixed: persistent volume template fixed, but not tested

--- config/mongo/init-router.js
+++ config/mongo/init-router.js
@@ -1,7 +1,3 @@
-sh.addShard("/:27018")
-sh.addShard("shard01/shard01b:27018")
-
-sh.addShard("shard02/shard02a:27019")
-sh.addShard("shard02/shard02b:27019")
-
+sh.addShard("mongo-shard0-0.shard0-service.myserver.svc.cluster.local:27017")
+sh.addShard("mongo-shard0-0.shard1-service.myserver.svc.cluster.local:27017")
 sh.enableSharding('test')
--- config/mongo/init-shard1.js
+++ config/mongo/init-shard1.js
@@ -4,7 +4,6 @@ rs.initiate(
       version: 1,
       members: [
          { _id: 0, host : "mongo-shard0-0.shard1-service.myserver.svc.cluster.local:27017" },
-         { _id: 1, host : "mongo-shard0-1.shard1-service.myserver.svc.cluster.local:27017" },
       ]
    }
 )
